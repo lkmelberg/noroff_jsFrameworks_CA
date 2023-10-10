b3263b9838b2c41432f567eadf051389d9ccda5b
lkmelberg <100446631+lkmelberg@users.noreply.github.com>
2023-10-10 18:29:18 +0200

    productPage done

---
+++ src/components/ProductDetails/index.jsx
@@ -0,0 +1,15 @@
+export function ProductDetails(product) {
+  //   const [openModal, setOpenModal] = useState(false);
+  //   const { increaseCartQuantity } = useShoppingCart();
+
+  //   const handleClick = () => {
+  //     increaseCartQuantity(product.id);
+  //     setOpenModal(true);
+  //   };
+  console.log(product);
+  return (
+    <>
+      <h2>{product.title}</h2>
+    </>
+  );
+}
--- src/render/Product/index.jsx
+++ src/render/Product/index.jsx
@@ -1,22 +1,172 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import { FetchProduct } from "../../api/FetchProduct";
+import React, { useEffect, useState } from "react";
+import { Routes, Route, Link, useParams } from "react-router-dom";
+import {
+  Box,
+  chakra,
+  Container,
+  Stack,
+  Text,
+  Image,
+  Flex,
+  VStack,
+  Button,
+  Heading,
+  SimpleGrid,
+  StackDivider,
+  VisuallyHidden,
+  List,
+  ListItem,
+} from "@chakra-ui/react";
+
 import { API_BASE_URL } from "../../api";
 
 export function Product() {
-  const { product, isLoading, isError } = FetchProduct(API_BASE_URL);
-  const { description, discountedPrice, id, imageUrl, title } = product;
-  if (isLoading) {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  let { id } = useParams();
+
+  useEffect(() => {
+    async function getData(url) {
+      try {
+        setIsLoading(true);
+        setIsError(false);
+
+        const response = await fetch(url);
+        const json = await response.json();
+
+        setData(json);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    getData(`${API_BASE_URL}/${id}`);
+  }, [id]);
+
+  if (isLoading || !data) {
     return <div>Loading</div>;
   }
 
   if (isError) {
     return <div>Error</div>;
   }
+
+  console.log(data);
+
+  const {
+    title,
+    description,
+    price,
+    discountedPrice,
+    imageUrl,
+    rating,
+    tags,
+    reviews,
+  } = data;
+
   return (
-    <>
-      <div>Individual product page: {id}</div>
-      <h1>{title}</h1>
-    </>
+    <Container maxW={"7xl"}>
+      <SimpleGrid
+        columns={{ base: 1, lg: 2 }}
+        spacing={{ base: 8, md: 10 }}
+        py={{ base: 18, md: 24 }}>
+        <Flex>
+          <Image
+            rounded={"md"}
+            alt={"product image"}
+            src={imageUrl}
+            fit={"cover"}
+            align={"center"}
+            w={"100%"}
+            h={{ base: "100%", sm: "400px", lg: "500px" }}
+          />
+        </Flex>
+        <Stack spacing={{ base: 6, md: 10 }}>
+          <Box as={"header"}>
+            <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              fontSize={{ base: "2xl", sm: "4xl", lg: "5xl" }}>
+              {title}
+            </Heading>
+            <Text
+              color={("gray.900", "gray.400")}
+              fontWeight={300}
+              fontSize={"2xl"}>
+              {discountedPrice} NOK
+            </Text>
+          </Box>
+
+          <Stack
+            spacing={{ base: 4, sm: 6 }}
+            direction={"column"}
+            divider={<StackDivider borderColor={("gray.200", "gray.600")} />}>
+            <VStack spacing={{ base: 4, sm: 6 }}>
+              <Text fontSize={"lg"}>{description}</Text>
+            </VStack>
+            tags
+            <Box>
+              <Text
+                fontSize={{ base: "16px", lg: "18px" }}
+                color={("pink.500", "pink.300")}
+                fontWeight={"500"}
+                textTransform={"uppercase"}
+                mb={"4"}>
+                Product Tags:
+              </Text>
+              <Text fontSize={"lg"}>
+                {tags.map((tag) => (
+                  <>
+                    <Text
+                      fontSize={{ base: "12px", lg: "14px" }}
+                      as={"li"}
+                      fontWeight={"light"}>
+                      {tag}
+                    </Text>
+                  </>
+                ))}
+              </Text>
+            </Box>
+            <Flex justifyContent="space-around">
+              <Button variant="solid" colorScheme="blue">
+                Add to cart
+              </Button>
+
+              <Stack
+                direction="row"
+                alignItems="center"
+                justifyContent={"center"}>
+                <Text>2-3 business days delivery</Text>
+              </Stack>
+            </Flex>
+            <Box>
+              <Text
+                fontSize={{ base: "16px", lg: "18px" }}
+                color={("pink.500", "pink.300")}
+                fontWeight={"500"}
+                textTransform={"uppercase"}
+                mb={"4"}>
+                Product Reviews:
+              </Text>
+              <Text fontSize={"lg"}>
+                {reviews.map((reviews) => (
+                  <>
+                    <Text as={"span"} fontWeight={"bold"}>
+                      {reviews.username}
+                    </Text>
+                    <span fontWeight={"light"}> {reviews.description}</span>
+                    <span fontWeight={"ultraLight"}> {reviews.rating}/5</span>
+                    <br />
+                  </>
+                ))}
+              </Text>
+            </Box>
+          </Stack>
+        </Stack>
+      </SimpleGrid>
+    </Container>
   );
 }
